bf8ade055937c8c7c8cc391e3cd021d9e4e58841
magdalenahuget <magdalena.huget@gmail.com>
2023-07-24 17:54:00 +0200

    Initial commit

---
+++ src/main/java/com/javastart/Library.java
@@ -0,0 +1,23 @@
+package com.javastart;
+
+public class Library {
+    public static void main(String[] args) {
+        final String appName = "Library v 0.1";
+
+        String title = "Harry Potter and the Philosopher's Stone";
+        String author = "J. K. Rowling";
+        int releaseDate = 1997;
+        int pages = 223;
+        String publisher ="Bloomsbury";
+        String isbn = "123456";
+
+        System.out.println(appName);
+        System.out.println("Available books:");
+        System.out.println(title);
+        System.out.println(author);
+        System.out.println(releaseDate);
+        System.out.println(pages);
+        System.out.println(publisher);
+        System.out.println(isbn);
+    }
+}
